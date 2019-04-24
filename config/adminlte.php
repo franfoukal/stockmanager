bac7d56cf74fb7075ea0e5a5e432dc5494681ccf
Fran Foukal <franfoukal@MacBook-Air-de-Usuario.local>
2019-04-24 05:55:11 -0300

    First Commit

---
+++ config/adminlte.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'title' => 'Stock Manager',
+    'logo' => 'nada',
+    'userPic' => './img/customer-support.png',
+    'username' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Skin adminLTE **VER**
+    |--------------------------------------------------------------------------
+    |   Cambia el look&feel de la plantilla, el 'tema'
+    |   los disponibles son: 
+    |       - blue, skyblue, black, purple, yellow, red, and green
+    |   Cada skin tiene la variante 'light' con el dashboard en claro:
+    |       - blue-light, black-light, etc.
+    |
+    */
+    
+    'sidebar' => '',
+    'navbar' => '',
+    'brand-logo' => '',
+
+];
