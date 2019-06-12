53ef59c70ed7cfe67778b299c06bcf218823cf83
Fran Foukal <franfoukal@MacBook-Air-de-Usuario.local>
2019-06-12 09:54:43 -0300

    role access to routes fixes

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::group(['middleware' => ['auth']], function(){
         Route::get('/materiales', function () {
             return view('modules/container')->with('component', 'materiales');
         });
-        Route::get('/materiales/listar', 'MaterialController@index');
+        
         Route::post('/materiales/agregar', 'MaterialController@store');
         Route::post('/materiales/actualizar', 'MaterialController@update');
         Route::delete('/materiales/eliminar/{id}', 'MaterialController@destroy');   
@@ -38,7 +38,7 @@ Route::group(['middleware' => ['auth']], function(){
         Route::get('/contratistas', function () {
             return view('modules/container')->with('component', 'contratistas');
         });
-        Route::get('/contratistas/listar', 'ContratistaController@index');
+        
         Route::post('/contratistas/agregar', 'ContratistaController@store');
         Route::post('/contratistas/actualizar', 'ContratistaController@update');
         Route::delete('/contratistas/eliminar/{id}', 'ContratistaController@destroy');
@@ -47,12 +47,17 @@ Route::group(['middleware' => ['auth']], function(){
         Route::get('/user', function () {
             return view('modules/container')->with('component', 'usuarios');
         });
-        Route::get('/user/list', 'UserController@index');
+        
         Route::get('/user/roles', 'UserController@getRoles');
         Route::post('/user/config', 'UserController@attachUser');
 
     });
-   
+
+    
+    Route::get('/user/list', 'UserController@index');
+    Route::get('/contratistas/listar', 'ContratistaController@index');
+    Route::get('/materiales/listar', 'MaterialController@index');
+    
     //Rutas para calendario
     Route::get('/calendario', function () {
         return view('modules/container')->with('component', 'full-calendar');
